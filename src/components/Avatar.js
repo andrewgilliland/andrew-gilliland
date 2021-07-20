9f4db54c047c6f2f14812243f799df4acaaab460
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-07-20 18:28:54 -0500

    adds rainbow avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
+// import { useStaticQuery, graphql } from "gatsby"
+// import Image from "gatsby-image"
 import styled from "styled-components"
 
 const AvatarStyles = styled.div`
@@ -8,34 +8,58 @@ const AvatarStyles = styled.div`
     border-radius: 100%;
     box-shadow: 2px 2px var(--pink-vivid-500);
   }
+
+  .rainbow {
+    background-image: linear-gradient(
+      to left,
+      violet 14%,
+      indigo 14% 28%,
+      blue 28% 42%,
+      green 42% 56%,
+      yellow 56% 70%,
+      orange 70% 84%,
+      red 84% 100%
+    );
+
+    height: 50px;
+    width: 50px;
+    border-radius: 9999px;
+    border: 3px solid white;
+    transition: all 0.3s ease;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
 `
 
 const Avatar = () => {
-  const data = useStaticQuery(graphql`
-    query AvatarQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-        childImageSharp {
-          fixed(width: 50, height: 50, quality: 100) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      site {
-        siteMetadata {
-          author {
-            name
-          }
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query AvatarQuery {
+  //     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+  //       childImageSharp {
+  //         fixed(width: 50, height: 50, quality: 100) {
+  //           ...GatsbyImageSharpFixed
+  //         }
+  //       }
+  //     }
+  //     site {
+  //       siteMetadata {
+  //         author {
+  //           name
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
-  const author = data.site.siteMetadata?.author
-  const avatar = data?.avatar?.childImageSharp?.fixed
+  // const author = data.site.siteMetadata?.author
+  // const avatar = data?.avatar?.childImageSharp?.fixed
 
   return (
     <AvatarStyles>
-      {avatar && <Image fixed={avatar} alt={author?.name || ``} />}
+      {/* {avatar && <Image fixed={avatar} alt={author?.name || ``} />} */}
+      <div className="rainbow"></div>
     </AvatarStyles>
   )
 }
